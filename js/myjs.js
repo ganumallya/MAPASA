68ba3dc16e68f896d3443f78f9ab44381bacfc06
ganumallya <ganumallya@yahoo.co.in>
2016-09-24 09:16:50 +0530

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
     
    $('section').css("min-height", $(window).height());
    $('.jumbotron').css("min-height", $(window).height());
+   
+    $('#loginmodalbutton').click(function(){
+        $('#signmodal').addClass("hide");
+        $('#loginmodal').removeClass("hide");
+    });
+                          
+    $('#signmodalbutton').click(function(){
+        $('#loginmodal').addClass("hide");
+        $('#signmodal').removeClass("hide");
+    });
     
      $(function() {
       $('a[href*="#section"]').click(function() {
